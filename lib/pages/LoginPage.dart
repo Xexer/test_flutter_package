509b53f0abf8b4050e004ec8a09e99ec015d9ed8
Xexer <xexer@web.de>
2020-05-02 17:23:32 +0200

    Initial commit

---
+++ lib/pages/LoginPage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:testflutterpackage/data/constants.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        backgroundColor: kMainColor,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.power_settings_new),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      backgroundColor: kMainBackgroundColor,
+      body: Center(
+        child: Text('Welcome'),
+      ),
+    );
+  }
+}
